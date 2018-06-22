fd9ae06639c06dc1893bebc3921b6efcb99f78b3
Sych Yury <sych_y@mail.ru>
2018-06-22 16:14:56 +0300

    add header

--- client/src/app/_services/authentication.service.ts
+++ client/src/app/_services/authentication.service.ts
@@ -1,6 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, BehaviorSubject} from 'rxjs';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
 
@@ -8,6 +8,8 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class AuthenticationService {
     constructor(private http: HttpClient) { }
+
+    public loggedIn = new BehaviorSubject<boolean>(false);
 
     login(username: string, password: string) {
         return this.http.post<any>(`${environment.serverUrl}auth/login`,
@@ -17,12 +19,14 @@ export class AuthenticationService {
                 if (res && res.token) {
                     // store username and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify({ username, token: res.token }));
+                    this.loggedIn.next(true);
                 }
             }));
     }
 
     logout() {
         // remove user from local storage to log user out
+        this.loggedIn.next(false);
         localStorage.removeItem('currentUser');
     }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,27 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md'
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
-
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
-
 import { AuthGuard } from './_guards';
 import { JwtInterceptor } from './_helpers';
 import {AlertService, AuthenticationService, UserService} from './_services';
 import { HomeComponent } from './home';
-import { LoginComponent } from './login';;
-import { AlertComponent } from './_directives/alert.component'
-;
-import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login';
+import { AlertComponent } from './_directives/alert.component';
+import { RegisterComponent } from './register/register.component';;
+import { HeaderComponent } from './shared/header/header.component'
+
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
+        MDBBootstrapModule.forRoot(),
         routing
     ],
     declarations: [
@@ -30,7 +30,10 @@ import { RegisterComponent } from './register/register.component'
         LoginComponent,
         AlertComponent ,
         RegisterComponent
+,
+        HeaderComponent
     ],
+  schemas: [],
     providers: [
         AuthGuard,
         AlertService,
